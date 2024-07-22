dc774e4c414832060308bf4b57e3b82da999de5f
lhdmir <lhdmir@naver.com>
2024-07-23 02:22:21 +0900

    태초마을

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,5 @@
-import "./css/App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import IntroductionPage from "./components/IntroductionPage.jsx";
-import LoginPage from "./components/LoginPage.jsx";
-import RegisterPage from "./components/RegisterPage.jsx";
-import MoguriSelectPage from "./components/MoguriSelectPage.jsx";
-import MoguriWeightPage from "./components/MoguriWeightPage.jsx";
-
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<IntroductionPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/moguri-select" element={<MoguriSelectPage />} />
-          <Route path="/moguri-weight" element={<MoguriWeightPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return;
 }
 
 export default App;
