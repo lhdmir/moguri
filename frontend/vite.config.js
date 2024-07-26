555282012a1488aee1f2bf9485981938c334b854
lhdmir <lhdmir@naver.com>
2024-07-26 13:53:41 +0900

    이미지 루트 경로 설정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// ESM 환경에서 __dirname 설정
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      assets: path.resolve(__dirname, "src/assets"),
+    },
+  },
 });
