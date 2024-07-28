58852e24eeb36d5413239f04e8f825db261f4980
lee seung heon <tmdgjs0011@naver.com>
2024-07-28 10:45:42 +0900

    슬라이드 식단 미완성

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,8 @@ import background1 from "../../assets/image/background1.png";
 import Nav from "../../components/Nav";
 import closeButtonImage from "../../assets/icon/deletebutton.png";
 import backButtonImage from "../../assets/icon/backbutton.png";
+import slideButtonImage from "../../assets/icon/slidebutton.png";
+import todoDeleteImage from "../../assets/icon/tododelete.png";
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -14,6 +16,16 @@ const Home = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const selectedMoguri = useSelector((state) => state.moguri.image);
   const targetWeight = useSelector((state) => state.moguri.target_weight);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const [currentView, setCurrentView] = useState("main"); // 'main' 또는 'meal'
+  const [currentMeal, setCurrentMeal] = useState("");
+  const [mealDetails, setMealDetails] = useState({
+    breakfast: [],
+    lunch: [],
+    dinner: [],
+    snack: [],
+  });
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -55,12 +67,152 @@ const Home = () => {
     return parts.join("/");
   };
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen); 
+    setCurrentView("main"); 
+  };
+
+  const openMealDetails = (meal) => {
+    setCurrentMeal(meal);
+    setCurrentView("meal");
+  };
+
+  const addMealItem = (meal) => {
+    const newMealDetails = { ...mealDetails };
+    newMealDetails[meal].push({ name: "", calories: 0 });
+    setMealDetails(newMealDetails);
+  };
+
+  const removeMealItem = (meal, index) => {
+    const newMealDetails = { ...mealDetails };
+    newMealDetails[meal].splice(index, 1);
+    setMealDetails(newMealDetails);
+  };
+
+  const handleMealItemChange = (meal, index, field, value) => {
+    const newMealDetails = { ...mealDetails };
+    newMealDetails[meal][index][field] = value;
+    setMealDetails(newMealDetails);
+  };
+
+  const handleCaloriesChange = (meal, index, value) => {
+    const numericValue = value.replace(/[^\d]/g, ""); // 숫자가 아닌 문자를 제거
+    handleMealItemChange(meal, index, "calories", numericValue);
+  };
+
+  const closeMealDetails = () => {
+    setCurrentView("main");
+    setIsSidebarOpen(false); // 창을 닫고 사이드바를 숨깁니다.
+  };
+
   return (
     <div
       className="home-container"
       style={{ backgroundImage: `url(${background1})` }}
     >
       <Nav />
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <button className="slide-button" onClick={toggleSidebar}>
+          <img src={slideButtonImage} alt="Slide" />
+        </button>
+        {isSidebarOpen &&
+          (currentView === "main" ? (
+            <div className="sidebar-content">
+              <div className="meal-header">오늘의 식단 !</div>
+              <button className="meal-main-close" onClick={toggleSidebar}>
+                <img
+                  src={closeButtonImage}
+                  alt="Close"
+                  className="meal-main-close"
+                />
+              </button>
+              <div
+                className="meal-item"
+                onClick={() => openMealDetails("breakfast")}
+              >
+                <p className="meal-item-day">• 아침</p>
+              </div>
+              <div
+                className="meal-item"
+                onClick={() => openMealDetails("lunch")}
+              >
+                <p className="meal-item-day">• 점심</p>
+              </div>
+              <div
+                className="meal-item"
+                onClick={() => openMealDetails("dinner")}
+              >
+                <p className="meal-item-day">• 저녁</p>
+              </div>
+              <div
+                className="meal-item"
+                onClick={() => openMealDetails("snack")}
+              >
+                <p className="meal-item-day">• 간식</p>
+              </div>
+            </div>
+          ) : (
+            <div className="meal-details">
+              <button
+                className="meal-details-back"
+                onClick={() => setCurrentView("main")}
+              >
+                <img src={backButtonImage} alt="Back" className="meal-detail-back" />
+              </button>
+              <button className="meal-details-close" onClick={closeMealDetails}>
+                <img
+                  src={closeButtonImage}
+                  alt="Close"
+                  className="meal-detail-close"
+                />
+              </button>
+              <div className="meal-header">{currentMeal}</div>
+              {mealDetails[currentMeal].map((item, index) => (
+                <div key={index} className="meal-item-detail">
+                  <input
+                    type="text"
+                    value={item.name}
+                    onChange={(e) =>
+                      handleMealItemChange(
+                        currentMeal,
+                        index,
+                        "name",
+                        e.target.value
+                      )
+                    }
+                    placeholder="메뉴 추가"
+                    className="meal-input" // 추가된 클래스
+                  />
+                  <input
+                    type="text"
+                    value={`${item.calories}kcal`}
+                    onChange={(e) =>
+                      handleCaloriesChange(
+                        currentMeal,
+                        index,
+                        e.target.value.replace("kcal", "").trim() // 'kcal'을 제거하고 숫자 부분만 넘김
+                      )
+                    }
+                    placeholder="0"
+                    className="meal-input"
+                  />
+                  <button
+                    className="meal-delete-button"
+                    onClick={() => removeMealItem(currentMeal, index)}
+                  >
+                    <img src={todoDeleteImage} alt="Delete" />
+                  </button>
+                </div>
+              ))}
+              <button
+                className="add-button"
+                onClick={() => addMealItem(currentMeal)}
+              >
+                + 메뉴 추가
+              </button>
+            </div>
+          ))}
+      </div>
       {selectedMoguri && (
         <img
           src={selectedMoguri}
@@ -132,7 +284,3 @@ Home.propTypes = {
 };
 
 export default Home;
-
-
-
-// 테스트
