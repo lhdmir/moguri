e2e38e785c1112fd60bd75c89af6f510b86aa2e0
lhdmir <lhdmir@naver.com>
2024-07-31 03:57:25 +0900

    13

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/moguri_fe/", // GitHub Pages 배포 경로로 설정
+  base: "/", // GitHub Pages 배포 경로로 설정
   plugins: [react()],
   resolve: {
     alias: {
