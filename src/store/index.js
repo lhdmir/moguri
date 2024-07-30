88a32c83b01557d67a49aaa9eb66dfa6ea618a34
lhdmir <42959665+lhdmir@users.noreply.github.com>
2024-07-31 02:07:28 +0900

    좆버그 해결 (#47)

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // 기본적으로 로컬 스토리지를 사용
 
-// Redux Store 생성
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
 
-export default store;
+const persistor = persistStore(store);
 
+export { store, persistor };
+export default store;
