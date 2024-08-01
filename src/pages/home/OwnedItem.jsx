0034fad0daab2e749d5bd6a4974cd6fc2e9eddd6
lhdmir <42959665+lhdmir@users.noreply.github.com>
2024-08-02 00:35:00 +0900

    OwnedItem 창 프로토타입 제작 (#77)

--- src/pages/home/OwnedItem.jsx
+++ src/pages/home/OwnedItem.jsx
@@ -1,3 +1,62 @@
-const OwnedItem = () => {};
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+
+import "./OwnedItem.css";
+
+const OwnedItem = () => {
+  const moguri = useSelector((state) => state.moguri);
+  const [selectedCategory, setSelectedCategory] = useState("accessories");
+
+  // 선택된 카테고리의 아이템들 가져오기
+  const items = moguri?.ownedItems[selectedCategory] || [];
+
+  // 아이템을 두 줄로 나누기
+  const firstRowItems = items.slice(0, 3);
+  const secondRowItems = items.slice(3, 6);
+  return (
+    <div>
+      <div className="item-header">{moguri.name}</div>
+      <div className="category-buttons">
+        <button
+          className={`category-button ${
+            selectedCategory === "accessories" ? "selected" : ""
+          }`}
+          onClick={() => setSelectedCategory("accessories")}
+        >
+          액세서리
+        </button>
+        <button
+          className={`category-button ${
+            selectedCategory === "backgrounds" ? "selected" : ""
+          }`}
+          onClick={() => setSelectedCategory("backgrounds")}
+        >
+          배경화면
+        </button>
+      </div>
+      <div className="item-container">
+        <div className="item-row">
+          {firstRowItems.map((item) => (
+            <div className="item" key={item.id}>
+              <img src={item.imageUrl} alt={item.name} />
+            </div>
+          ))}
+        </div>
+        <div className="item-row2">
+          {secondRowItems.map((item) => (
+            <div className="item" key={item.id}>
+              <img src={item.imageUrl} alt={item.name} />
+            </div>
+          ))}
+          {selectedCategory === "accessories" && (
+            <div className="item unmount-item">
+              <div>장착 해제</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default OwnedItem;
