41c61a8762bd2e8babe70dc85d667c4fee6962c2
lee seung heon <tmdgjs0011@naver.com>
2024-08-01 12:07:48 +0900

    아이템 보관함 ui 완성

--- src/pages/home/ItemSelection.jsx
+++ src/pages/home/ItemSelection.jsx
@@ -1,23 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { useSelector } from "react-redux"; // 리덕스 훅 임포트
+import { useSelector } from "react-redux";
 import closeButtonImage from "../../assets/icon/deletebutton.png";
 import slideButtonImage from "../../assets/icon/slidebutton.png";
 import "./ItemSelection.css";
 
 const ItemSelection = ({ goToPreviousView, goToNextView, toggleSidebar }) => {
-  const moguriName = useSelector((state) => state.moguri.name); // 리덕스 스토어에서 moguriName 가져오기
+  const moguri = useSelector((state) => state.moguri);
+  const [selectedCategory, setSelectedCategory] = useState("accessories");
+
+  // 선택된 카테고리의 아이템들 가져오기
+  const items = moguri?.ownedItems[selectedCategory] || [];
+
+  // 아이템을 두 줄로 나누기
+  const firstRowItems = items.slice(0, 3);
+  const secondRowItems = items.slice(3, 6);
 
   return (
     <div className="item-selection">
       <button className="slide-left" onClick={goToPreviousView}>
         <img src={slideButtonImage} alt="Slide" />
       </button>
-      <div className="item-header">{moguriName}</div> {/* 모구리 이름 표시 */}
+      <div className="item-header">{moguri.name}</div>
       <button className="meal-main-close" onClick={toggleSidebar}>
         <img src={closeButtonImage} alt="Close" className="meal-main-close" />
       </button>
-      <div className="item-container">{/* 아이템 내용 보류 */}</div>
+      <div className="category-buttons">
+        <button
+          className={`category-button ${
+            selectedCategory === "accessories" ? "selected" : ""
+          }`}
+          onClick={() => setSelectedCategory("accessories")}
+        >
+          액세서리
+        </button>
+        <button
+          className={`category-button ${
+            selectedCategory === "backgrounds" ? "selected" : ""
+          }`}
+          onClick={() => setSelectedCategory("backgrounds")}
+        >
+          배경화면
+        </button>
+      </div>
+      <div className="item-container">
+        <div className="item-row">
+          {firstRowItems.map((item) => (
+            <div className="item" key={item.id}>
+              <img src={item.imageUrl} alt={item.name} />
+            </div>
+          ))}
+        </div>
+        <div className="item-row2">
+          {secondRowItems.map((item) => (
+            <div className="item" key={item.id}>
+              <img src={item.imageUrl} alt={item.name} />
+            </div>
+          ))}
+          {selectedCategory === "accessories" && (
+            <div className="item unmount-item">
+              <div>장착 해제</div>
+            </div>
+          )}
+        </div>
+      </div>
       <button className="slide-right" onClick={goToNextView}>
         <img src={slideButtonImage} alt="Slide" />
       </button>
@@ -32,3 +78,4 @@ ItemSelection.propTypes = {
 };
 
 export default ItemSelection;
+
