7ae78a776037bc60b2725c93f0893d4edfa0164c
lhdmir <lhdmir@naver.com>
2024-08-04 02:29:35 +0900

    로그아웃 기능, 토큰 만료 기간 수정

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 import "../components/Nav.css";
-import "../assets/image/banner.png"
+import "../assets/image/banner.png";
 
 const Nav = () => {
+  const navigate = useNavigate();
+
+  // const handleLogout = async () => {
+  //   const token = Cookies.get("token");
+  //   console.log(token);
+
+  //   if (!token) {
+  //     console.error("Token not found");
+  //     return;
+  //   }
+
+  //   try {
+  //     const response = await fetch(
+  //       "https://5797b8a7-4933-4b3c-b62d-53e86f8c48ef.mock.pstmn.io/logout",
+  //       {
+  //         method: "POST",
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     );
+
+  //     if (response.ok) {
+  //       navigate("/");
+  //     } else {
+  //       console.error("Logout failed");
+  //     }
+  //   } catch (error) {
+  //     console.error("An error occurred:", error);
+  //   }
+  // };
+
+  const handleLogout = () => {
+    navigate("/");
+  };
+
   return (
     <div className="nav-container">
       <div className="nav-left">
         <span className="moguri-banner">MOGURI</span>
       </div>
-        <div className="nav-container-logout">
-          <span className="nav-logout">Logout</span>
-        </div>
-        <div className="nav-container-banner">
-          <span className="community-banner">community</span>
-        </div>
+      <div className="nav-container-logout" onClick={handleLogout}>
+        <span className="nav-logout">Logout</span>
       </div>
+      <div className="nav-container-banner">
+        <span className="community-banner">community</span>
+      </div>
+    </div>
   );
 };
 
