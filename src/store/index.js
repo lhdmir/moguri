c3a7391783c77424e651d5a5cb01715caa4fcc32
lhdmir <lhdmir@naver.com>
2024-08-04 16:10:08 +0900

    [error] A non-serializable value was detected in an action, in the path: `register`.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
-import { persistReducer, persistStore } from "redux-persist";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage"; // 기본적으로 로컬 스토리지를 사용
 
 const persistConfig = {
@@ -12,6 +21,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistor = persistStore(store);
